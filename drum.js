9214d2b313b87e2398e22e144055364af933d7dc
singhadityapratap964-ai <singhadityapratap964@gmail.com>
2026-01-14 22:44:05 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -1,6 +1,8 @@
-for(var i=0;i<document.querySelectorAll("btn").length;i++){
+var btn=document.querySelectorAll("btn")
+for(var i=0;i<btn.length;i++){
 document.querySelectorAll("button")[i].addEventListener("click",function (){
     alert("clicked")
     console.log("h")
 });
 }
+
