08391a4883474aae60ac4400f0a406e5fac4e110
singhadityapratap964-ai <singhadityapratap964@gmail.com>
2026-01-15 00:58:36 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -1,10 +1,53 @@
-var btn = document.querySelectorAll(".btn")
-for(var i=0;i<btn.length;i++){
-document.querySelectorAll(".btn")[i].addEventListener("click",()=>{
-    alert("clicked")
-    console.log("h")
-});
+var btn = document.querySelectorAll(".drum")
+for (var i = 0; i < btn.length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var innerHTML = this.innerHTML
+        makeSound(innerHTML)
+    });
 }
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key)
+})
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            var w = new Audio('tom-1.mp3')
+            w.play()
+            break;
+        case 'a':
+            var a = new Audio('tom-2.mp3')
+            a.play();
+            break;
+
+        case 's':
+            var s = new Audio('tom-3.mp3')
+            s.play();
+            break;
+
+        case 'd':
+            var d = new Audio('tom-4.mp3')
+            d.play();
+            break;
+
+        case 'j':
+            var j = new Audio('snare.mp3')
+            j.play();
+            break;
+
+        case 'k':
+            var k = new Audio('crash.mp3')
+            k.play();
+            break;
+
+        case 'l':
+            var l = new Audio('kick-bass.mp3')
+            l.play();
+            break;
+    }
+};
+
+
 
 
 
