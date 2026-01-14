af8931d7b2bb6daeed971f55f27c9e3084575cd0
singhadityapratap964-ai <singhadityapratap964@gmail.com>
2026-01-14 22:40:23 +0530

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,6 @@
+for(var i=0;i<document.querySelectorAll("btn").length;i++){
+document.querySelectorAll("button")[i].addEventListener("click",function (){
+    alert("clicked")
+    console.log("h")
+});
+}
